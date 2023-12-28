123904518769ac28d783d60a5e1c71b1c7d8469b
rameshbommidi <rameshbommidi125@gmail.com>
2023-12-28 14:20:10 +0000

    done

---
+++ src/ListContainer/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+import ListItem from '../ListItem'
+
+class ListContainer extends Component {
+  render() {
+    const {historyList, deleteListItem} = this.props
+
+    return (
+      <div>
+        <ul className="lists-container">
+          {historyList.map(eachObject => (
+            <ListItem
+              deleteListItem={deleteListItem}
+              eachObject={eachObject}
+              key={eachObject.id}
+            />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default ListContainer
